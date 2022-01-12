1824d99a38d37a8b983b11bba6fc265c3335cf89
e.okur <e.okur@campus.tu-berlin.de>
2022-01-12 19:21:41 +0100

    LSTM

---
+++ LSTM.py
@@ -0,0 +1,63 @@
+### Data Collection
+import pandas_datareader as pdr
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+### Create the Stacked LSTM model
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
+
+key=""
+
+
+df = pdr.DataReader("AAPL",
+                       start='2010-12-1',
+                       end='2022-01-10',
+                       data_source='yahoo')
+df.to_csv('AAPL.csv')
+df=pd.read_csv('AAPL.csv')
+df1=df.reset_index()['Close']
+
+scaler=MinMaxScaler(feature_range=(0,1))
+df1=scaler.fit_transform(np.array(df1).reshape(-1,1))
+
+plt.plot(df1)
+#plt.show()
+
+##splitting dataset into train and test split
+training_size=int(len(df1)*0.65)
+test_size=len(df1)-training_size
+train_data,test_data=df1[0:training_size,:],df1[training_size:len(df1),:1]
+
+print(training_size,test_size)
+
+def create_dataset(dataset, time_step=1):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-time_step-1):
+		a = dataset[i:(i+time_step), 0]   ###i=0, 0,1,2,3-----99   100
+		dataX.append(a)
+		dataY.append(dataset[i + time_step, 0])
+	return np.array(dataX), np.array(dataY)
+
+# reshape into X=t,t+1,t+2,t+3 and Y=t+4
+time_step = 100
+X_train, y_train = create_dataset(train_data, time_step)
+X_test, ytest = create_dataset(test_data, time_step)
+
+print()
+print(X_train.shape)
+
+# reshape input to be [samples, time steps, features] which is required for LSTM
+X_train =X_train.reshape(X_train.shape[0],X_train.shape[1] , 1)
+X_test = X_test.reshape(X_test.shape[0],X_test.shape[1] , 1)
+
+model=Sequential()
+model.add(LSTM(50,return_sequences=True,input_shape=(100,1)))
+model.add(LSTM(50,return_sequences=True))
+model.add(LSTM(50))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error',optimizer='adam')
+
+model.summary()
